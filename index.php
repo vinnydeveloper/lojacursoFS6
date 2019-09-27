65c840d8d21a924ee9cba5640ac4bbe24ac5d74d
Vinicius Oliveira <vinicius.developer@outlook.com>
2019-09-27 12:09:08 -0300

    Iniciando projeto

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    $nomeSistema = "Cursos Ai!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <header class="d-flex justify-content-between align-items-center p-3">
+        <h1 id="logo">
+            <?php echo $nomeSistema; ?> 
+        </h1>
+
+        
+        <nav>
+            <ul class="nav">
+                <li class="nav-item">Cursos</li>
+                <li class="nav-item">Login</li>
+                <li class="nav-item">Cadastrar</li>
+            </ul>
+        </nav>
+</header>
+    
+</body>
+</html>
